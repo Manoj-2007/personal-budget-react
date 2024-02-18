c1e42fc8faa2a239b8d6857aedadab06944329b2
Manoj Aitha <93203846+Manoj-2007@users.noreply.github.com>
2024-02-18 09:15:57 -0500

    Add files via upload
    Second Commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom" role="contentinfo">
+        <div className="center">
+            All rights reserved &copy; Vamsi Sidda
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
